a38c39ff2477e84de661c2c425a8ccd96289b76e
Sumeet Jadhav <sjadhav@launchcg.com>
2024-03-03 10:55:45 +0530

    added React router dom

--- src/App.tsx
+++ src/App.tsx
@@ -3,21 +3,29 @@ import './App.css';
 import Navbar from './components/Navbar/Navbar';
 import Sidebar from './components/Sidebar/Sidebar';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Dashboard from './components/Dashboard/Dashboard';
 // https://www.youtube.com/watch?v=nUUsUAPEjFc&ab_channel=OnlineTutorials
 function App() {
   const [navbarToggleClick, setNavbarToggleClick] = useState(false);
   return (
-    <div className="App">
-      <Navbar navbarToggleClick={navbarToggleClick} setNavbarToggleClick={setNavbarToggleClick} />
-      <div className="main-section">
-        <div>
-          <Sidebar navbarToggleClick={navbarToggleClick} />
-        </div>
-        <div className={`main-container ${navbarToggleClick ? 'minimized-navbar' : ''}`}>
-          <div className="container m-2 full-height">Container</div>
+    <BrowserRouter>
+      <div className="App">
+        <Navbar navbarToggleClick={navbarToggleClick} setNavbarToggleClick={setNavbarToggleClick} />
+        <div className="main-section">
+          <div>
+            <Sidebar navbarToggleClick={navbarToggleClick} />
+          </div>
+          <div className={`main-container ${navbarToggleClick ? 'minimized-navbar' : ''}`}>
+            <div className="container m-2 full-height">
+              <Routes>
+                <Route path="/" element={<Dashboard />}></Route>
+              </Routes>
+            </div>
+          </div>
         </div>
       </div>
-    </div>
+    </BrowserRouter>
   );
 }
 
